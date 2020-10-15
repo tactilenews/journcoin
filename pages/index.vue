fe1927e62b4ee8d18d641ccf31d4debefb163082
roschaefer <git@roschaefer.de>
2020-10-15 19:32:26 +0200

    ci: Deploy to vercel

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title class="headline"> Welcome to JournCoin </v-card-title>
+        <v-card-text>
+          <p>Journcoin is a new digital currency for journalism.</p>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" nuxt to="#"> Continue </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
---
+++ vuetify.options.js
@@ -0,0 +1,9 @@
+export default {
+  customVariables: ['~/assets/variables.scss'],
+  defaultAssets: {
+    font: {
+      family: 'Roboto',
+    },
+    icons: 'mdiSvg',
+  },
+}
