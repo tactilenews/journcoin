bd35da57e62805139fb29274e9508a3893a9379a
roschaefer <git@roschaefer.de>
2020-11-02 16:45:03 +0100

    feat: test validateJournCoin

--- components/QrCodeScanner/QrCodeScanner.spec.js
+++ components/QrCodeScanner/QrCodeScanner.spec.js
@@ -1,9 +1,72 @@
-import { mount } from '@vue/test-utils'
-import QrCodeScanner from './QrCodeScanner.vue'
+import { createLocalVue, mount, shallowMount } from '@vue/test-utils'
+import VueQrcodeReader from 'vue-qrcode-reader'
+import QrCodeScanner, { validJournCoin } from './QrCodeScanner.vue'
+
+const localVue = createLocalVue()
+localVue.use(VueQrcodeReader)
+
+describe('validJournCoin', () => {
+  describe('given URL', () => {
+    const urls = {
+      'https://journcoin.org/coin/42': '42',
+      'https://journcoin.org/coin/abcde': 'abcde',
+      'https://journcoin.org/wrong/route/42': false,
+      whatever: false,
+      'https://some-random-domain.org/coin/42': false,
+    }
+
+    for (const url in urls) {
+      it(`returns ${urls[url]} for ${url}`, () => {
+        expect(validJournCoin(url)).toEqual(urls[url])
+      })
+    }
+
+    it('returns false for undefined', () => {
+      expect(validJournCoin(undefined)).toEqual(false)
+    })
+
+    it('returns false for null', () => {
+      expect(validJournCoin(null)).toEqual(false)
+    })
+  })
+})
 
 describe('QrCodeScanner', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(QrCodeScanner)
-    expect(wrapper.vm).toBeTruthy()
+  describe('mount', () => {
+    it('is a Vue instance', () => {
+      const wrapper = mount(QrCodeScanner, { localVue })
+      expect(wrapper.vm).toBeTruthy()
+    })
+  })
+
+  describe('shallowMount', () => {
+    describe('when QR is scanned', () => {
+      let url
+      const qrCodeScanned = async () => {
+        const wrapper = shallowMount(QrCodeScanner, { localVue })
+        await wrapper
+          .findComponent({ name: 'qrcode-stream' })
+          .vm.$emit('decode', url)
+        return wrapper
+      }
+
+      describe('QR code is invalid', () => {
+        beforeEach(() => (url = 'https://journcoin.org/42'))
+
+        it('emits no event', async () => {
+          const wrapper = await qrCodeScanned()
+          expect(wrapper.emitted()).toEqual({})
+        })
+      })
+
+      describe('QR code is valid', () => {
+        beforeEach(() => (url = 'https://journcoin.org/coin/abcde'))
+
+        it('$emits "earn"', async () => {
+          const wrapper = await qrCodeScanned()
+          expect(wrapper.emitted('earn')).toEqual([['abcde']])
+        })
+      })
+    })
   })
 })
--- components/QrCodeScanner/QrCodeScanner.vue
+++ components/QrCodeScanner/QrCodeScanner.vue
@@ -1,16 +1,30 @@
 <template>
   <div class="h-64 w-64 bg-red-600">
     <client-only>
-      <!--<qrcode-stream class="m-auto" @decode="onDecode"></qrcode-stream>-->
+      <qrcode-stream class="m-auto" @decode="onDecode"></qrcode-stream>
     </client-only>
   </div>
 </template>
 
 <script>
+export const validJournCoin = (decodedString) => {
+  try {
+    const url = new URL(decodedString)
+    if (url.hostname !== 'journcoin.org' || !url.pathname.startsWith('/coin'))
+      return false
+    const [, , ...tail] = url.pathname.split('/')
+    return tail.join('/')
+  } catch (e) {
+    return false
+  }
+}
+
 export default {
   methods: {
     onDecode(decodedString) {
-      console.log('decodedString', decodedString)
+      const coin = validJournCoin(decodedString)
+      if (!coin) return false
+      this.$emit('earn', coin)
     },
   },
 }
