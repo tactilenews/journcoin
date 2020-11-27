3182ec830185981d7efab6b081437432c72aa81b
roschaefer <git@roschaefer.de>
2020-11-27 14:27:48 +0100

    feat: setup persisted state across page reloads

--- webapp/nuxt.config.js
+++ webapp/nuxt.config.js
@@ -23,7 +23,10 @@ export default {
   css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [{ src: '~/plugins/qrCodeReader.js', mode: 'client' }],
+  plugins: [
+    { src: '~/plugins/qrCodeReader.client.js' },
+    { src: '~/plugins/persistedState.client.js' },
+  ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
